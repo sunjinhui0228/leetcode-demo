1549ad75a6dd6c427bb59e0967ba965b2900b283
sunjinhui <1327623829@qq.com>
2021-06-26 13:48:50 +0800

    两数相加

--- leetcodeDemo/src/main/java/com/leetcode/demo/leetcode/Leetcode02.java
+++ leetcodeDemo/src/main/java/com/leetcode/demo/leetcode/Leetcode02.java
@@ -1,40 +1,59 @@
 package com.leetcode.demo.leetcode;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author sunjianjian
- * @date 2021年06月${日}0:00
+ * @date 2021年06月${日}15:18
  */
-@RestController
+@Controller
 @RequestMapping("/Leetcode02")
 public class Leetcode02 {
 
-    /**例：19的二进制100011 返回3
-     * 求输入数字转化为2进制中1的个数
-     * @author sunjianjian
-     * @date 2021/6/23 19:32
-     * @param n
-     * @return int
-     */
-    @RequestMapping("/hammingWeight")
-    public int hammingWeight(int n) {
-        int res = 0;
-        while (n != 0){
-            res += n & 1; //若n&1 =1（则n的二进制最后一位为1） 则统计数res + 1
-            n >>>= 1; //将二进制的数字无符号n 右移一位（去掉最一位）
-        }
-        return res;
+    //链表
+    public class ListNode {
+        int val;        //定义val变量值，存储节点值
+        ListNode next;  //定义next指针，指向下一个节点，位置节点连接
+        ListNode() {};
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
-    @RequestMapping("/hammingWeight02")
-    public int hammingWeight02(int n) {
-        int res = 0;
-        while (n != 0){
-            res++;
-            n &= n-1; //消去数字 n 最右边的 1
+    /**
+     * 两数相加
+     * @author sunjianjian
+     * @date 2021/6/26 11:46
+     * @param l1
+     * @param l2
+     * @return com.leetcode.demo.utils.ListNode
+     */
+    @RequestMapping("/addTwoNumbers")
+    public ListNode addTwoNumbers(ListNode l1,ListNode l2){
+        ListNode head = null, tail = null;
+        int carry = 0; //进位值
+        while (l1 != null || l2 != null){
+            //把两个链表的同一位赋值给n1,n2
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry; //和
+            if (head == null){
+                head = tail = new ListNode(sum % 10); //取余
+            }else {
+                tail.next = new ListNode(sum % 10);
+                tail =  tail.next;
+            }
+            carry = sum / 10;  //进位值
+            if (l1 != null){
+                l1 = l1.next;
+            }
+            if (l2 != null){
+                l2 = l2.next;
+            }
         }
-        return res;
+        if (carry > 0){
+            tail.next = new ListNode(carry);
+        }
+        return head;
     }
 }
